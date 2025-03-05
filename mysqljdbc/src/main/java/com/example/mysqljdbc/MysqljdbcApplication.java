daec8669fcbba7a26d115541252c1e79f8a9c3c4
anj1eC0des <129589930+anj1eC0des@users.noreply.github.com>
2025-03-05 14:28:42 +0530

    Add files via upload

---
+++ mysqljdbc/src/main/java/com/example/mysqljdbc/MysqljdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqljdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqljdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqljdbcApplication.class, args);
+	}
+
+}
